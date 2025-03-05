054ce9be3ade0fefb4759e6873d833c81e310d88
Prateek0P <114237183+Prateek0P@users.noreply.github.com>
2025-03-05 09:55:51 +0530

    Update 25.java

--- Week 3/25.java
+++ Week 3/25.java
@@ -1,1 +1,9 @@
+import java.util.Arrays;
 
+public class SortStrings {
+    public static void main(String[] args) {
+        String[] words = {"banana", "apple", "cherry", "date"};
+        Arrays.sort(words);
+        System.out.println("Sorted words: " + Arrays.toString(words));
+    }
+}
