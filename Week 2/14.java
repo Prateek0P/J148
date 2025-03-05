2ff6c25ecc0170d5b2f392c52629e538cd6ce42a
Prateek0P <114237183+Prateek0P@users.noreply.github.com>
2025-03-05 09:52:37 +0530

    Update 14.java

--- Week 2/14.java
+++ Week 2/14.java
@@ -1,1 +1,14 @@
 
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10, a = 0, b = 1, next;
+        System.out.print("Fibonacci Series: " + a + " " + b);
+        for (int i = 2; i < n; i++) {
+            next = a + b;
+            System.out.print(" " + next);
+            a = b;
+            b = next;
+        }
+    }
+}
+
