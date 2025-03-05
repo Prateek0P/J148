12c7a738d72b5a66543b5f35d1728b1cb563b360
Prateek0P <114237183+Prateek0P@users.noreply.github.com>
2025-03-05 09:55:12 +0530

    Update 23.java

--- Week 3/23.java
+++ Week 3/23.java
@@ -1,1 +1,9 @@
 
+public class CharArrayToString {
+    public static void main(String[] args) {
+        char[] charArray = {'H', 'e', 'l', 'l', 'o'};
+        String str = new String(charArray);
+        System.out.println("String: " + str);
+    }
+}
+
