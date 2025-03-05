defbc09e82670a816b666fba41e3743d85373717
Prateek0P <114237183+Prateek0P@users.noreply.github.com>
2025-03-05 09:52:06 +0530

    Update 12.java

--- Week 2/12.java
+++ Week 2/12.java
@@ -1,1 +1,16 @@
 
+public class PrimeCheck {
+    public static void main(String[] args) {
+        int num = 29;
+        boolean isPrime = num > 1;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        System.out.println(num + " is " + (isPrime ? "a Prime Number" : "not a Prime Number"));
+    }
+}
+
+
