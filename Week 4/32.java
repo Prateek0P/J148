425462b8e6011fd27063a95ff0fc5e3a9912853e
Prateek0P <114237183+Prateek0P@users.noreply.github.com>
2025-03-05 09:57:58 +0530

    Update 32.java

--- Week 4/32.java
+++ Week 4/32.java
@@ -1,1 +1,9 @@
 
+public class ReplaceDigits {
+    public static void main(String[] args) {
+        String str = "H3ll0 W0rld";
+        String result = str.replaceAll("[0-9]", "*");
+        System.out.println("Modified string: " + result);
+    }
+}
+
