a170c89822555dece7f89fed0cdd54d44078a321
Prateek0P <114237183+Prateek0P@users.noreply.github.com>
2025-03-05 09:48:25 +0530

    Update 2.java

--- Week 1/2.java
+++ Week 1/2.java
@@ -1,1 +1,8 @@
-
+public class CompoundInterest {
+    public static void main(String[] args) {
+        double principal = 1000, rate = 5, time = 3;
+        double amount = principal * Math.pow((1 + rate / 100), time);
+        double ci = amount - principal;
+        System.out.println("Compound Interest: " + ci);
+    }
+}
