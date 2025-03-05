3a4fb478bd02e343fb05d33dc906a965a1abe5a0
Prateek0P <114237183+Prateek0P@users.noreply.github.com>
2025-03-05 09:50:16 +0530

    Update 7.java

--- Week 1/7.java
+++ Week 1/7.java
@@ -1,1 +1,9 @@
 
+public class AsciiValue {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int ascii = (int) ch;
+        System.out.println("ASCII value of " + ch + " is: " + ascii);
+    }
+}
+
